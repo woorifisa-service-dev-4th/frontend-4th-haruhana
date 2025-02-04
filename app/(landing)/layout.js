566287d9d30c545eb21efedafa12e4f9527b7b2b
namsh1125 <namsh1125@naver.com>
2025-02-04 16:14:40 +0900

    💄 랜딩 페이지 UI/UX 수정

---
+++ app/(landing)/layout.js
@@ -0,0 +1,5 @@
+import "../globals.css";
+
+export default function RootLayout({ children }) {
+  return <div className="min-h-screen">{children}</div>;
+}
---
+++ constants/landing/companies.js
@@ -0,0 +1,8 @@
+export const COMPANIES = [
+  "naver",
+  "kakao",
+  "line",
+  "coupang",
+  "carrotmarket",
+  "wooribank",
+];
